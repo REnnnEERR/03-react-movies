3526498651499fe0a1bf71cfb2cb3cf26043a5a3
Ivan BOLTENKOV <ivanwrest@gmail.com>
2026-03-19 23:04:09 +0200

    fix interface

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,18 +1,20 @@
 import axios from 'axios';
-import type { FetchMoviesResponse } from '../types/movie';
+import type { Movie } from '../types/movie'; 
+
+interface FetchMoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`;
 
-export const fetchMovies = async (query: string): Promise<FetchMoviesResponse> => {
-  const response = await axios.get<FetchMoviesResponse>('/search/movie', {
-    params: {
-      query,
-      include_adult: false,
-      language: 'en-US',
-      page: 1,
-    },
+export const fetchMovies = async (page: number = 1): Promise<FetchMoviesResponse> => {
+    const { data } = await axios.get<FetchMoviesResponse>('/trending/movie/day', {
+    params: { page }
   });
-  return response.data;
+  return data;
 };
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -8,9 +8,9 @@ export interface Movie {
   vote_average: number;
 }
 
-export interface FetchMoviesResponse {
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-  page: number;
-}
+// export interface FetchMoviesResponse {
+//   results: Movie[];
+//   total_pages: number;
+//   total_results: number;
+//   page: number;
+// }
