849c803638a8ed839438fe79f9a139c49d6eba1d
Ivan BOLTENKOV <ivanwrest@gmail.com>
2026-03-19 23:16:02 +0200

    fix interface

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -12,9 +12,14 @@ const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`;
 
-export const fetchMovies = async (page: number = 1): Promise<FetchMoviesResponse> => {
-    const { data } = await axios.get<FetchMoviesResponse>('/trending/movie/day', {
-    params: { page }
+export const fetchMovies = async (query: string, page: number = 1): Promise<FetchMoviesResponse> => {
+  const { data } = await axios.get<FetchMoviesResponse>('/search/movie', {
+    params: { 
+      query: query,
+      page: page, 
+      language: 'uk-UA',
+      include_adult: false
+    }
   });
   return data;
 };
